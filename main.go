f663ed809d88d6cb2ffaec2bb2c8244b270d9d14
OB <lanreboris11@yahoo.com>
2022-06-20 18:36:39 +0000

    handles get requests for articles (route: /view/{id})

--- main.go
+++ main.go
@@ -3,8 +3,6 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
@@ -18,24 +16,11 @@ func main() {
 	// Load the templates
 	router.LoadHTMLGlob("templates/*")
 
-	// Define the route for the index page and display the index.html template
-	// To start with, we'll use an inline route handler. Later on, we'll create
-	// standalone functions that will be used as route handlers.
-	router.GET("/", func(c *gin.Context) {
+	// Handle Index
+	router.GET("/", showIndexPage)
 
-		// Call the HTML method of the Context to render a template
-		c.HTML(
-			// Set the HTTP status to 200 (OK)
-			http.StatusOK,
-			// Use the index.html template
-			"index.html",
-			// Pass the data that the page uses (in this case, 'title')
-			gin.H{
-				"title": "Home Page",
-			},
-		)
-
-	})
+	// Handle GET requests at /article/view/some_article_id
+	router.GET("/article/view/:article_id", getArticle)
 
 	// Start serving the application - port 8080 by default
 	router.Run()
